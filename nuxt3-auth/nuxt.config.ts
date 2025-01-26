d119155cd82d62e3596e5da9455d4c7ee657097c
bahlool1401 <bahlool.ramesh@gmail.com>
2025-01-26 23:54:31 +0330

    add nuxt3 module (tailwind)

--- nuxt3-auth/nuxt.config.ts
+++ nuxt3-auth/nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
-  modules:[
-    '@nuxtjs/tailwindcss'
-  ]
+  devtools: { enabled: true }
 })
