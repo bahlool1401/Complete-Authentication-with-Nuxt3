7b7fa664ea4866fed7c9c9be173d117f820d510a
bahlool1401 <bahlool.ramesh@gmail.com>
2025-02-01 23:42:47 +0330

    add button (register or login) and set which one is in each time

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -3,7 +3,15 @@
         <div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
             <form action="" class="space-y-6">
                 <!-- Input -->
-                 <Input :disabled="isLoading" required id="name" label="Name" v-model="name"/>
+                 <Input :disabled="isLoading" required id="name" label="Name" v-model="name" type="text" v-if="variant==='REGISTER'"/>
+                 <Input :disabled="isLoading" required id="email" label="Email" v-model="email" type="email"/>
+                 <Input :disabled="isLoading" required id="password" label="Password" v-model="password" type="password"/>
+
+                 <div>
+                    <Button :disabled="isLoading" fullWidth type="submit">
+                        {{ variant==='LOGIN' ? 'Sign In' : 'Register' }}
+                    </Button>
+                 </div>
             </form>
         </div>
     </div>
@@ -12,4 +20,9 @@
 <script setup lang="ts">
     const isLoading = ref(false)
     const name = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    type VARIANT = 'LOGIN' | 'REGISTER'
+    const variant = ref<VARIANT>('REGISTER')
 </script>
---
+++ components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button :type="type" :disabled="disabled" 
+    :class="
+    clsx(`flex justify-center rounded-md px-3 py-2 font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2` , disabled && 'opacity-50 cursor-default' , fullWidth && 'w-full',
+    secondary ? 'text-gray-900' : 'text-white',
+    danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+    !secondary && !danger && 'bg-sky-600 focus-visible:outline-sky-600'  
+    )
+    ">
+    <slot/>
+    </button>
+</template>
+
+<script setup lang="ts">
+import clsx from 'clsx';
+
+interface ButtonProps{
+    type?:'button' | 'submit' | 'reset' | undefined;
+    fullWidth?:boolean;
+    secondary?:boolean;
+    danger?:boolean;
+    disabled?:boolean;
+}
+const props= defineProps<ButtonProps>();
+</script>
