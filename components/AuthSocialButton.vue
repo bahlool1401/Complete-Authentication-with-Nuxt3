3b7b2fdebc6f26c37b57da593556b3aee77f4681
bahlool1401 <bahlool.ramesh@gmail.com>
2025-02-05 23:52:35 +0330

    test

--- components/AuthSocialButton.vue
+++ components/AuthSocialButton.vue
@@ -1,6 +1,7 @@
 <template>
     <button type="button" class="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0">
         <Icon :name="icon"/>
+        
     </button>
 </template>
 
