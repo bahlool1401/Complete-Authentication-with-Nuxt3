c1c7d44ea5e893f7cf8bff4d008880452cde862f
bahlool1401 <bahlool.ramesh@gmail.com>
2025-02-17 23:37:47 +0330

    add submit func for register

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div class="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-            <form action="" class="space-y-6">
+            <form action="" class="space-y-6" @submit.prevent="onSubmit">
                 <!-- Input -->
-                 <Input :disabled="isLoading" required id="name" label="Name" v-model="name" type="text" v-if="variant==='REGISTER'"/>
-                 <Input :disabled="isLoading" required id="email" label="Email" v-model="email" type="email"/>
-                 <Input :disabled="isLoading" required id="password" label="Password" v-model="password" type="password"/>
+                <Input :disabled="isLoading" required id="name" label="Name" v-model="name" type="text"
+                    v-if="variant === 'REGISTER'" />
+                <Input :disabled="isLoading" required id="email" label="Email" v-model="email" type="email" />
+                <Input :disabled="isLoading" required id="password" label="Password" v-model="password"
+                    type="password" />
 
-                 <div>
+                <div>
                     <Button :disabled="isLoading" fullWidth type="submit" outline>
-                        {{ variant==='LOGIN' ? 'Sign In' : 'Register' }}
+                        {{ variant === 'LOGIN' ? 'Sign In' : 'Register' }}
                     </Button>
-                 </div>
-                 <div class="mt-6">
+                </div>
+                <div class="mt-6">
                     <div class="relative">
                         <div class="absolute inset-0 flex items-center">
                             <div class="w-full border-t border-gray-300"></div>
@@ -22,19 +24,19 @@
                         </div>
                     </div>
                     <div class="mt-6 flex gap-2">
-                        <AuthSocialButton icon="bi:github"/>
-                        <AuthSocialButton icon="bi:google"/>
+                        <AuthSocialButton icon="bi:github" />
+                        <AuthSocialButton icon="bi:google" />
                     </div>
-                 </div>
+                </div>
 
-                 <div class="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+                <div class="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
                     <div @click="toggleVariant">
-                        {{ variant==='REGISTER' ? 'Already have an account?' : 'New to Messenger?' }}
+                        {{ variant === 'REGISTER' ? 'Already have an account?' : 'New to Messenger?' }}
                     </div>
                     <div @click="toggleVariant" class="underline cursor-pointer ">
-                        {{ variant==='LOGIN' ? 'Create an account?' : 'Login' }}
+                        {{ variant === 'LOGIN' ? 'Create an account?' : 'Login' }}
                     </div>
-                 </div>
+                </div>
 
             </form>
             variant : {{ variant }}
@@ -43,19 +45,48 @@
 </template>
 
 <script setup lang="ts">
-    const isLoading = ref(false);
-    const name = ref('');
-    const email = ref('');
-    const password = ref('');
+const isLoading = ref(false);
+const name = ref('');
+const email = ref('');
+const password = ref('');
 
-    type VARIANT = 'LOGIN' | 'REGISTER';
-    const variant = ref<VARIANT>('REGISTER')
+type VARIANT = 'LOGIN' | 'REGISTER';
+const variant = ref<VARIANT>('REGISTER')
 
-    const toggleVariant = ()=>{
-        if(variant.value==='REGISTER'){
-            variant.value='LOGIN';
-        }else{
-            variant.value='REGISTER';
+const onSubmit = async () => {
+    if (variant.value === 'REGISTER') {
+        try {
+            isLoading.value = true
+            const { data, error } = await useFetch('/api/auth/register', {
+                body: {
+                    name: name.value,
+                    password: password.value,
+                    email: email.value
+                }
+            })
+
+            if (error.value) {
+                console.log(error.value);
+            }
+            if(data.value){
+                console.log('Successfully Rrgistered');
+            }
+        } catch (error) {
+
+        } finally {
+            isLoading.value = false
         }
+    } else {
+        console.log('Login');
     }
+}
+
+
+const toggleVariant = () => {
+    if (variant.value === 'REGISTER') {
+        variant.value = 'LOGIN';
+    } else {
+        variant.value = 'REGISTER';
+    }
+}
 </script>
