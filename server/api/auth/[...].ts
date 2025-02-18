f210ae9682098d826ca89deaa1be573bf09e5aaa
bahlool1401 <bahlool.ramesh@gmail.com>
2025-02-18 23:51:48 +0330

    edit in configuration (sidebase-nuxtAuth)

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -1,27 +1,38 @@
-import { NuxtAuthHandler } from '#auth'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
+import { NuxtAuthHandler } from "#auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NuxtAuthHandler({
   // your authentication configuration here!
 
-  secret: 'your-secret-here',
+  secret: "your-secret-here",
   providers: [
     // @ts-expect-error Use .default here for it to work during SSR.
     GithubProvider.default({
-      clientId: 'your-client-id',
-      clientSecret: 'your-client-secret'
+      clientId: "your-client-id",
+      clientSecret: "your-client-secret",
     }),
     //@ts-expect-error
     GoogleProvider.default({
-      clientId: 'your-client-id',
-      clientSecret: 'your-client-secret'
+      clientId: "your-client-id",
+      clientSecret: "your-client-secret",
     }),
 
-//@ts-expect-error
+    //@ts-expect-error
     CredentialsProvider.default({
-        name: Credentials
-    })
-  ]
-
-})
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "email" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials:any){
+        if(credentials?.email || credentials?.password){
+          throw createError({
+            statusCode:500,
+            statusMessage:'Missing Info'
+          })
+        }
+      }
+    }),
+  ],
+});
